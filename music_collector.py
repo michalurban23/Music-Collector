ba172f4398fb7c5e51ccbcbd6a10a13e73857cad
Michał Urban <michalurban23@gmail.com>
2017-05-02 23:45:58 +0200

    minor changes for clean code

--- music_collector.py
+++ music_collector.py
@@ -1,13 +1,12 @@
 import csv
 import os
-import sys
 import os.path
 import time
 import random
 
 
-# Main menu of the program (what user sees)
-def menu():
+def display_menu():
+    """Main menu of program (what user sees), returns foolproofed user input"""
     os.system("clear")
     print("Welcome to the music collector. Choose your action: \n")
     print(" 1) Add new album to collection")
@@ -21,12 +20,12 @@ def menu():
     print(" 9) Show the total amount of albums by an artist")
     print("10) Find the longest album")
     print(" 0) Exit")
-    pick = input_choice()
-    return pick
+    user_choice = check_input_choice()
+    return user_choice
 
 
-# Foolproof input choice:
-def input_choice():
+def check_input_choice():
+    """Checks menu input for foolproofing"""
     while True:
         option = input("\nSelect an option: ")
         if option not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]:
@@ -36,28 +35,32 @@ def input_choice():
             return option
 
 
-# Read and save entry file as a tuple of 2 tuples
 def read_from_file():
+    """Reads whole database from file and returns it as a list of tuples"""
     albums = []
     with open('music.csv', newline='') as csvfile:
         content = csv.reader(csvfile, delimiter='|')
         for line in content:
-            albums.append(
-                ((line[0].strip().title(), line[1].strip().title()),
-                 (int(line[2]), line[3].strip().lower(), line[4].strip())))
+            if len(line) == 5:  # To prevent blank lane at the end of file from wrecking havoc
+                albums.append(
+                    ((line[0].strip().title(), line[1].strip().title()),
+                     (int(line[2]), line[3].strip().lower(), line[4].strip())))
     return albums
 
 
-# Adding a new album to the collection:
-def add_new(all_albums):
+def add_new():
+    """Adds new album to the collection"""
     print("Here you can add a new album to collection.")
     print("You can type 'exit' any time to go back to menu\n")
+
     artist = input("Enter artist name:\n")
     if artist == "exit":
         return
+
     album = input("Enter album name:\n")
     if album == "exit":
         return
+
     while True:
         year = input("Enter year of release:\n")
         if year == "exit":
@@ -68,10 +71,13 @@ def add_new(all_albums):
         except ValueError:
             print("Enter year in YYYY format:\n")
             continue
+
     genre = input("Enter music genre:\n")
     if genre == "exit":
         return
+
     while True:
+        # Will check if length is entered in correct format nn:nn, n is number
         length = input("Enter length of the album:\n")
         if length == "exit":
             return
@@ -79,6 +85,7 @@ def add_new(all_albums):
             break
         else:
             print("Next time enter length as MM:SS\n")
+
     with open('music.csv', 'a', newline='') as csvfile:
         content = csv.writer(csvfile, delimiter='|')
         content.writerow([artist.title(), album.title(), year, genre.lower(), length])
@@ -86,8 +93,8 @@ def add_new(all_albums):
     time.sleep(2)
 
 
-# Find an album by an artist:
-def album_by_artist(all_albums):
+def find_album_by_artist(all_albums):
+    """Takes in list of albums, searches for specific artist, prints matching"""
     while True:
         zero_match = 0
         artist = input("Enter name of an artist you're looking for (or 'exit'): \n")
@@ -97,15 +104,15 @@ def album_by_artist(all_albums):
         for data in all_albums:
             if data[0][0] == artist.title():
                 zero_match += 1
-                print(str(zero_match) + (") ") + str(data[0][1]))
+                print(str(zero_match) + ") " + str(data[0][1]))
                 print("   Year: " + str(data[1][0]) + ", Genre: " + str(data[1][1]) + ", Length: " + str(data[1][2]))
         if zero_match == 0:
             print("Sorry. Couldn't find this artist on our list. Try again.")
         print("")
 
 
-# Find an album by year:
-def album_by_year(all_albums):
+def find_album_by_year(all_albums):
+    """Takes in list of albums, searches for specific year, prints matching"""
     while True:
         zero_match = 0
         year = input("Enter the year you're looking for (or 'exit'): \n")
@@ -120,15 +127,15 @@ def album_by_year(all_albums):
         for data in all_albums:
             if data[1][0] == year:
                 zero_match += 1
-                print(str(zero_match) + (") ") + str(data[0][1]))
+                print(str(zero_match) + ") " + str(data[0][1]))
                 print("   Artist: " + str(data[0][0]) + ", Genre: " + str(data[1][1]) + ", Length: " + str(data[1][2]))
         if zero_match == 0:
             print("Sorry. No album in database was released that year. Try again.")
         print("")
 
 
-# Find an artist by album:
-def artist_by_album(all_albums):
+def find_artist_by_album(all_albums):
+    """Takes in list of albums, searches for specific album, prints matching artist"""
     while True:
         zero_match = 0
         album = input("Enter name of an album you're looking for (or 'exit'): \n")
@@ -138,15 +145,15 @@ def artist_by_album(all_albums):
         for data in all_albums:
             if data[0][1] == album.title():
                 zero_match += 1
-                print(str(zero_match) + (") ") + str(data[0][0]))
+                print(str(zero_match) + ") " + str(data[0][0]))
                 print("   Year: " + str(data[1][0]) + ", Genre: " + str(data[1][1]) + ", Length: " + str(data[1][2]))
         if zero_match == 0:
             print("Sorry. Couldn't find this album on our list. Try again.")
         print("")
 
 
-# Find and album by letters:
-def album_by_letter(all_albums):
+def find_album_by_letter(all_albums):
+    """Takes in list of albums, searches for letters in albums, prints matching"""
     while True:
         zero_match = 0
         letters = input("Enter any sequence of characters to search for the album\n \
@@ -157,15 +164,15 @@ def album_by_letter(all_albums):
         for data in all_albums:
             if letters.lower() in data[0][1].lower():
                 zero_match += 1
-                print(str(zero_match) + (") ") + str(data[0][1]) + "  by  " + str(data[0][0]))
+                print(str(zero_match) + ") " + str(data[0][1]) + "  by  " + str(data[0][0]))
                 print("   Year: " + str(data[1][0]) + ", Genre: " + str(data[1][1]) + ", Length: " + str(data[1][2]))
         if zero_match == 0:
             print("Sorry. Couldn't find any album containing those characters. Try again.")
         print("")
 
 
-# Find an album by its genre:
-def album_by_genre(all_albums):
+def find_album_by_genre(all_albums):
+    """Takes in list of albums, searches for specific genre, prints matching"""
     while True:
         zero_match = 0
         genre = input("Enter the genre you're looking for (or 'exit'): \n")
@@ -175,42 +182,41 @@ def album_by_genre(all_albums):
         for data in all_albums:
             if data[1][1] == genre.lower():
                 zero_match += 1
-                print(str(zero_match) + (") ") + str(data[0][1]) + "  by  " + str(data[0][0]))
+                print(str(zero_match) + ") " + str(data[0][1]) + "  by  " + str(data[0][0]))
                 print("   Year: " + str(data[1][0]) + ", Length: " + str(data[1][2]))
         if zero_match == 0:
             print("Sorry. No album in database from that genre. Try again.")
         print("")
 
 
-# Age of albums:
 def calculate_age(all_albums):
+    """Takes in list of albums, calculates their age in years, prints out total and average"""
     current_year = int(time.strftime("%Y"))
     print("There is a total of", len(all_albums), "albums in the database\n")
-    show_more = input("Type anything to calculate their average age (or 'exit'):\n")
+    show_more = input("Type anything to calculate their average age (or 'exit' to leave):\n")
     if show_more == "exit":
         return
     avg_age = 0
-    for year in all_albums:
-        avg_age += current_year - year[1][0]
+    for data in all_albums:
+        avg_age += current_year - data[1][0]
     avg_age /= len(all_albums)
-    print("\nIts year {} now, meaning average age of the albums is {} years\n".format(current_year, avg_age))
-    show_all = input("Type anything to list all the albums by their age (or 'exit'):\n")
+    print("\nIts year {} now, meaning average age of the albums is {:.5} years\n".format(current_year, avg_age))
+    show_all = input("Type anything to list all the albums by their age (or 'exit' to leave):\n")
     if show_all == "exit":
         return
     print("")
     sorting_index = 1
     for data in all_albums:
-        print(str(sorting_index) + (") ") + str(data[0][1]) + "  by  " + str(data[0][0]))
+        print(str(sorting_index) + ") " + str(data[0][1]) + "  by  " + str(data[0][0]))
         print("  ", current_year - data[1][0], "years old")
         sorting_index += 1
-    back_to_menu = input("\nType anything to return to main menu\n")
+    input("\nType anything to return to main menu\n")
 
 
-# Picks a random album by genre:
-def random_by_genre(all_albums):
+def pick_random_by_genre(all_albums):
+    """Takes in list of albums, searches for specific genre, prints random match"""
     while True:
-        zero_match = 0
-        genre = input("Enter the genre you're looking for (or 'exit') \n \
+        genre = input("Enter the genre you're looking for (or 'exit') to leave \n \
                       \rTIP: this will include all the sub-genres, meaning that typing 'metal'\n \
                       \r     will search through heavy metal, death metal, trash metal, etc.\n")
         if genre == "exit":
@@ -230,8 +236,8 @@ def random_by_genre(all_albums):
         print("")
 
 
-# Total amount of albums by an artist:
-def total_albums_by_artist(all_albums):
+def find_total_albums_by_artist(all_albums):
+    """Takes in list of albums, searches for specific artist, prints all matching albums"""
     while True:
         zero_match = 0
         artist = input("Enter name of an artist you're looking for (or 'exit'): \n")
@@ -248,8 +254,8 @@ def total_albums_by_artist(all_albums):
         print("")
 
 
-# Find the longest album in the database:
-def longest_album(all_albums):
+def find_longest_album(all_albums):
+    """Takes in list of albums, prints the longest album"""
     print("Here you can find out what is the longest album in our database")
     user_choice = input("Type 'exit' to return to main menu or anything else to continue\n")
     if user_choice == "exit":
@@ -257,7 +263,7 @@ def longest_album(all_albums):
     current_longest = 0
     for data in all_albums:
         length_in_seconds = 0
-        # This is to make sure inputs like 1:20:31 are understood same as 80:31
+        # This is to make sure inputs like 1:24:31 are understood same as 84:31, and 2:51:36 same as 171:36
         try:
             if data[1][2][-6] == ":":
                 length_in_seconds = 3600*int(data[1][2][:-6]) + 60*int(data[1][2][-5:-3]) + int(data[1][2][-2:])
@@ -274,29 +280,40 @@ def longest_album(all_albums):
     input("\nPress enter to return to main menu\n")
 
 
-# Main program (what user doesnt see):
-while True:
-    user_choice = menu()
-    os.system("clear")
-    if user_choice == "1":
-        add_new(read_from_file())
-    elif user_choice == "2":
-        album_by_artist(read_from_file())
-    elif user_choice == "3":
-        album_by_year(read_from_file())
-    elif user_choice == "4":
-        artist_by_album(read_from_file())
-    elif user_choice == "5":
-        album_by_letter(read_from_file())
-    elif user_choice == "6":
-        album_by_genre(read_from_file())
-    elif user_choice == "7":
-        calculate_age(read_from_file())
-    elif user_choice == "8":
-        random_by_genre(read_from_file())
-    elif user_choice == "9":
-        total_albums_by_artist(read_from_file())
-    elif user_choice == "10":
-        longest_album(read_from_file())
-    else:
-        sys.exit()
+def main():
+    try:
+        all_albums = read_from_file()
+    except FileNotFoundError:
+        print("\nMissing CSV file. Please download it from the repository.\n")
+        exit()
+
+    while True:
+        user_choice = display_menu()
+        os.system("clear")
+        if user_choice == "1":
+            add_new()
+            all_albums = read_from_file()
+        elif user_choice == "2":
+            find_album_by_artist(all_albums)
+        elif user_choice == "3":
+            find_album_by_year(all_albums)
+        elif user_choice == "4":
+            find_artist_by_album(all_albums)
+        elif user_choice == "5":
+            find_album_by_letter(all_albums)
+        elif user_choice == "6":
+            find_album_by_genre(all_albums)
+        elif user_choice == "7":
+            calculate_age(all_albums)
+        elif user_choice == "8":
+            pick_random_by_genre(all_albums)
+        elif user_choice == "9":
+            find_total_albums_by_artist(all_albums)
+        elif user_choice == "10":
+            find_longest_album(all_albums)
+        else:
+            exit()
+
+
+if __name__ == '__main__':
+    main()
